aa8bcd360768c8e7900848a5b60e1b52cc066fd1
stolenwise <lewis.r.stone@gmail.com>
2025-09-01 22:30:54 -0700

    update again

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavBar from "./components/Navbar";
+import NavBar from "./components/NavBar";
 import ApiTest from "./components/apitest";
 import CompanyList from "./components/Company/companylist";
 import CompanyDetails from "./components/Company/companydetails";
